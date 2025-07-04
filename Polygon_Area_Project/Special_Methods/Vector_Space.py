44d36503a467d58d35b19b9897904101f0ee3d26
Marius Francois Grassman <marius-jnr@outlook.com>
2025-07-04 17:40:18 +0200

    Refactor R2Vector class: streamline norm method, enhance string representation, and add demonstration of attribute access

--- Polygon_Area_Project/Special_Methods/Vector_Space.py
+++ Polygon_Area_Project/Special_Methods/Vector_Space.py
@@ -2,12 +2,20 @@ class R2Vector:
     def __init__(self, *, x, y):
         self.x = x
         self.y = y
-        
+
     def norm(self):
-        return (sum(val**2 for val in vars(self).values()))**0.5
-        
+        return sum(val**2 for val in vars(self).values())**0.5
+
     def __str__(self):
         return str(tuple(getattr(self, i) for i in vars(self)))
+
+    def __repr__(self):
+        arg_list = [f'{key}={val}' for key, val in vars(self).items()]
+        args = ', '.join(arg_list)
+        return f'{self.__class__.__name__}({args})'
+    
+    #def __getattribute__(self, attr):                
+    #   return 'calling __getattribute__'
 
 class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
@@ -16,5 +24,9 @@ class R3Vector(R2Vector):
 
 v1 = R2Vector(x=2, y=3)
 v2 = R3Vector(x=2, y=2, z=3)
-print(f"v1 = {v1}")  # Output: v1 = (2, 3)
-print(f"v2 = {v2}")  # Output: v2 = (2, 2, 3)
+
+# Demonstrating __getattribute__ calls
+print(f'Accessing x with dot operator: {v1.x}')
+print(f'Accessing x with getattr(): {getattr(v1, "x")}')
+#print(v1.z)
+#print(getattr(v1, 'z'))
